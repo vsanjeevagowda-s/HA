49649ffb5393ff5f335361b65205ff196b560088
vsanjeevagowda <vsanjeevagowda@qwinix.io>
2017-05-05 05:38:02 +0530

    initial setup commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gulpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gulpApp
+ */
+angular.module('gulpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
